f4705581da5433136f8aec3f3cd7ca0d02f5c3db
Muhammad Muzammil <122248440+Muzammil-404@users.noreply.github.com>
2023-10-19 13:10:27 +0500

    Update MAFIA.py

--- MAFIA.py
+++ MAFIA.py
@@ -1,13 +1,8 @@
 import os,platform
 os.system('git pull')
-os.system('rm -rf Mafia.so')
 MAFIA=platform.architecture()[0]
 if MAFIA=="64bit":
-     if not os.path.isfile('Mafia.so'):
-        os.system('curl -L https://github.com/Muzammil-404/MAFIA/blob/main/Mafia.cpython-311.so?raw=true -o Mafia.so') 
-        import Mafia
-     else:
-        import Mafia 
+     import Mafia
 elif MAFIA=="32bit":
     print(' Your Device is not support this tool ...')
     exit()

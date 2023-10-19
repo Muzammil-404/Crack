3955306296de6788bda0cf67f21117ee7fc35964
Muhammad Muzammil <122248440+Muzammil-404@users.noreply.github.com>
2023-10-19 12:37:49 +0500

    Update MAFIA.py

--- MAFIA.py
+++ MAFIA.py
@@ -3,7 +3,7 @@ os.system('git pull')
  
 MAFIA=platform.architecture()[0]
 if MAFIA=="64bit":
-     __import__("Mafia")
+     __import__("KHAN")
 elif MAFIA=="32bit":
     print('Sorry 32 Bit Not Supported...')
     exit()
